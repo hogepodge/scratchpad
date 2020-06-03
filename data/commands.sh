04a9908d50800d6f1f4edb181caf99d39933dc54
Chris Hoge <chris@hogepodge.com>
2020-06-03 14:29:32 -0700

    Final performance data

---
+++ data/commands.sh
@@ -0,0 +1,10 @@
+sudo apt install iperf3 sysstat -y
+
+# iperf testing from master
+git clone https://github.com/Pharb/kubernetes-iperf3.git
+cd kubernetes-iperf3
+./iperf3.sh -t 100 
+
+# cpu measurements from all nodes
+mpstat 2 10000
+
